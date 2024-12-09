89a8214e8736cb655f393063d1cc3eef5672d38e
Ilya Gordon <ilyagdn@gmail.com>
2024-12-08 17:29:24 -0800

    chore(hardhat): initialize project with basic setup

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    polygon: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      polygon: process.env.POLYGONSCAN_API_KEY
+    }
+  },
+  sourcify: {
+    enabled: false
+  }
+};
+
+
